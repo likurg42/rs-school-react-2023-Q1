5d9d9d9e8add5c7143ead7c850c4664f2fd07fe9
likurg42 <likurg42@gmail.com>
2023-03-19 23:02:42 +0300

    chore: update base path in config

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rs-school-react-2023-Q1/react-app/',
+  base: '/',
   test: {
     globals: true,
     environment: 'jsdom',
