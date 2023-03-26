f28919dd70077669e44223fcff3af9d54defeae6
likurg42 <likurg42@gmail.com>
2023-03-26 23:51:41 +0300

    feat: add profile card test

---
+++ react-app/src/components/Profiles/ProfileCard/ProfileCard.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import ProfileCard from './ProfileCard';
+import { ProfileModel } from '../../../types/profile.model';
+
+const mockProfile: ProfileModel = {
+  id: '1',
+  name: 'John Smitt',
+  primaryLanguage: 'Javascript',
+  opensource: true,
+  experience: 'Junior',
+  avatarUrl: 'https://picsum.photos/100/100',
+  birthDate: new Date(),
+  githubUrl: 'https://github.com/',
+};
+
+
+describe('profile card', () => {
+  it('should render profile card', () => {
+    render(<ProfileCard profile={mockProfile}/>);
+    const name = screen.queryByText(/John smitt/i);
+    expect(name).toBeVisible();
+  });
+});
