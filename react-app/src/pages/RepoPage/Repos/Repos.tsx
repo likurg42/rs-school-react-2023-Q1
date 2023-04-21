7115237312ea6c1a45c204ad692e808c4e859690
likurg42 <likurg42@gmail.com>
2023-04-21 22:09:51 +0300

    chore: use swc-react

--- react-app/src/pages/RepoPage/Repos/Repos.tsx
+++ react-app/src/pages/RepoPage/Repos/Repos.tsx
@@ -1,14 +1,13 @@
-// import useRepoContext from '../../../hooks/useRepoContext';
 import { RepoList } from '../RepoList/RepoList';
 import { buildSearchParams } from '../../../common/routes';
-import { useGetReposQuery } from '../../../store/services/repoApi';
+import { useGetReposQuery } from '../../../store/services';
 import { LoadingSpinner } from '../../../components/LoadingSpinner/LoadingSpinner';
 import { useRepositories } from '../../../hooks/useRepositories';
 
 const Repos = () => {
   const { currentFilter } = useRepositories();
   const {
-    data, isSuccess, isFetching, isError
+    data, isSuccess, isFetching, isError,
   } = useGetReposQuery(buildSearchParams(currentFilter));
   return (
     <>
